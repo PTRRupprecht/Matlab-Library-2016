047db4f583c5c8d068f70093dd216d57bedd9771
Peter Rupprecht <peter.rupprecht@fmi.ch>
2016-03-04 04:09:15 +0100

    setfig3 from Rainer
    makes all figures look the same (Arial)

---
+++ useful scripts/setfig3.m
@@ -0,0 +1,30 @@
+function setfig3(linw,fontsize,AbsTickLength)
+
+if nargin<3;
+    AbsTickLength=[];
+end
+if nargin<2;
+    fontsize=[];
+end
+if nargin<1;
+    linw=[];
+end
+
+if isempty(fontsize);
+    fontsize=10;
+end
+if isempty(linw);
+    linw=1;
+end
+if isempty(AbsTickLength);
+    AbsTickLength=0.004;
+end
+
+set(gca,'tickdir','out','fontsize',fontsize);
+pos=get(gca,'position');
+longaxis=max(pos(3:4));
+tickfactor=AbsTickLength/longaxis;
+set(gca,'ticklength',[tickfactor,0.025],'Box','off','linewidth',linw);
+
+
+set(gca,'fontname','Arial');
