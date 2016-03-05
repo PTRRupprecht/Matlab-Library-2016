af4c55f1aaead926f89d0110c7bdab9bd6728e89
PTRRupprecht <peter.rupprecht@fmi.ch>
2016-03-05 22:01:54 +0100

    Update Gilad OB analysis

--- useful scripts/extractAVG_fromManyTrials.m
+++ useful scripts/extractAVG_fromManyTrials.m
@@ -1,8 +1,8 @@
 
 
-FileX = dir('fish*X');
+FileX = dir('1603*_gilad');
 
-for jj = 1:numel(FileX)
+for jj = 2:numel(FileX)
     cd(FileX(jj).name);
     FileZ = dir('*odor*.tif');
     for kkk = 1:numel(FileZ)
@@ -19,11 +19,11 @@ for jj = 1:numel(FileX)
 end
 
 
-for jj = 1:numel(FileX)
+for jj = 3%:numel(FileX)
     for pp = 1:4
-        filename = strcat('Fish',FileX(jj).name,'plane',num2str(pp),'.tif');
-        imwrite(uint16(squeeze(AVG(:,:,pp,1,jj))),filename);
-        for kkk = 2:16
+        filename = strcat('FishII_Lys',FileX(jj).name,'plane',num2str(pp),'.tif');
+        imwrite(uint16(squeeze(AVG(:,:,pp,11,jj))),filename);
+        for kkk = 12:20
             imwrite(uint16(squeeze(AVG(:,:,pp,kkk,jj))),filename,'WriteMode','append');
         end
     end
